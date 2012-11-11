ba38647d80ccc6318d9c8f95fdcc54daf10af850
Manoj Mahalingam <manojlds@gmail.com>
2012-11-11 22:57:36 +0530

    Adding sample tests in real usage using Microsoft Fakes

---
+++ src/Toggler.TestHelper.UnitTests/ToggledFeatureTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.QualityTools.Testing.Fakes;
+using NUnit.Framework;
+
+namespace Toggler.TestHelper.UnitTests
+{
+    public class ToggledFeature : IToggled
+    {
+        public int SomeProcessing()
+        {
+            return this.IsOn() ? 1 : 2;
+        }
+    }
+
+    [TestFixture]
+    public class ToggledFeatureTests
+    {
+        [Test]
+        public void ShouldReturnOneWhenToggledFeatureIsOn()
+        {
+            var toggledFeature = new ToggledFeature();
+
+            using (ShimsContext.Create())
+            {
+                Fakes.ShimToggledExtensions.IsOnIToggled = toggled => true;
+                Assert.That(toggledFeature.SomeProcessing(), Is.EqualTo(1));
+            }
+        }
+
+        [Test]
+        public void ShouldReturnTwoWhenToggledFeatureIsOff()
+        {
+            var toggledFeature = new ToggledFeature();
+
+            using (ShimsContext.Create())
+            {
+                Fakes.ShimToggledExtensions.IsOnIToggled = toggled => false;
+                Assert.That(toggledFeature.SomeProcessing(), Is.EqualTo(2));
+            }
+        }
+    }
+}
